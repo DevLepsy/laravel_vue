3c20b17aae7c58fea7958edfae0d9e6e0e6e1aea
DevLepsy <70275748+DevLepsy@users.noreply.github.com>
2022-01-30 20:55:27 +0100

    add create router vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,20 @@
-require('./bootstrap');
+require("./bootstrap");
 
-import Alpine from 'alpinejs';
+import Alpine from "alpinejs";
 
 window.Alpine = Alpine;
 
 Alpine.start();
+
+import { createApp } from "vue";
+
+import router from "./router";
+import CustomerIndex from "./components/CustomerIndex.vue";
+
+createApp({
+    components: {
+        CustomerIndex
+    },
+})
+.use(router)
+.mount('#app');
